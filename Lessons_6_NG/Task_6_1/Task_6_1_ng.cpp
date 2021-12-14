a794d3dd4717283c3fe6434c4be4c1f7bedfdd5a
Kostya <kostazadoroznij9@gmail.com>
2021-12-14 23:40:31 +0200

    I complete Task

---
+++ Lessons_6_NG/Task_6_1/Task_6_1_ng.cpp
@@ -0,0 +1,25 @@
+﻿#include "CesareanCode.h"
+
+int main()
+{
+    string str = "";
+    int key = 0;
+    cout << "Enter the string you want to encode : ";
+    getline(cin, str);
+    cout << "Enter key : ";
+    cin >> key;
+    
+    CesareanCode *сesarean = new CesareanCode();
+    сesarean -> SetStr(str);
+    сesarean -> SetKey(key);
+
+    if (сesarean->GetStr() != "" || сesarean->GetKey() > 0) {
+        cout << сesarean->Encryption() << endl;
+        cout << сesarean->Decryption() << endl;
+    }
+    else cout << "ERROR" << endl;
+
+    cout << сesarean->Decryption("Euax zgyq bkxe otzkxkyzotm!", 6) << endl;
+    
+    delete сesarean;
+}
